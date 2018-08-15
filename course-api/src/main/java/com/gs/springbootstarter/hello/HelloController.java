55655b032d960969b19b9a9d382a26c48c75c57a
sarwar <sarwar@sarwar-pc>
2018-08-16 01:14:54 +0530

    spring boot basic java brain tutorials

---
+++ course-api/src/main/java/com/gs/springbootstarter/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.gs.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHai()
+	{
+		return "Hi";
+	}
+
+}
