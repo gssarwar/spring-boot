5b37c04415cf6026bfa1ba2a8c07fd39a1575440
sarwar <sarwar@sarwar-pc>
2018-08-16 01:26:04 +0530

    demo how to create spring boot broject

---
+++ course-api_unit3/src/main/java/io/gs/springbootquickstarter/courseapi/CourseApiUnit3Application.java
@@ -0,0 +1,12 @@
+package io.gs.springbootquickstarter.courseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiUnit3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiUnit3Application.class, args);
+	}
+}
